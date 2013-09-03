e99dce0a2d9e6bec6aa110dea8964ca3f830d07a
chiragck9 <chirag_ck9@hotmail.com>
2013-09-03 20:07:39 +0530

    project and apk

---
+++ TravelSearch/src/com/test/travelsearch/AutoCompleteAdapter.java
@@ -0,0 +1,170 @@
+package com.test.travelsearch;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Currency;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
+import android.util.JsonReader;
+import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+
+public class AutoCompleteAdapter extends ArrayAdapter<String> implements Filterable{
+
+	private static String RESULT_URL = "http://pre.dev.goeuro.de:12345/api/v1/suggest/position/en/name/";
+	private ArrayList<String> mResultList;
+	private static Context mContext;
+	
+	public AutoCompleteAdapter(Context context, int textViewResourceId) {
+		super(context, textViewResourceId);
+		mContext = context;
+	}
+
+	@Override
+	public int getCount() {
+		return mResultList.size();
+	}
+
+	@Override
+	 public Filter getFilter() {
+        Filter filter = new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults filterResults = new FilterResults();
+                if (constraint != null) {
+                    // Get the auto complete results.
+                    mResultList = getResultList(constraint.toString());
+                    
+                    // Assign the data to the FilterResults
+                    filterResults.values = mResultList;
+                    filterResults.count = mResultList.size();
+                }
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                if (results != null && results.count > 0) {
+                    notifyDataSetChanged();
+                }
+                else {
+                    notifyDataSetInvalidated();
+                }
+            }};
+        return filter;
+    }
+
+
+	@Override
+	public String getItem(int position) {
+		return mResultList.get(position);
+	}
+	
+	private static ArrayList<String> getResultList(String input){
+		ArrayList<String> cityNameList = null;
+		Map<Float,String> map= new HashMap<Float, String>();
+		Location currentLocation = null;
+		
+		// Method to return JSON data from the URL as a string
+        String jsonResults = getJsonAsString(RESULT_URL, input);
+		
+		try {
+			JSONObject jsonObject = new JSONObject(jsonResults);
+			JSONArray jsonArray = jsonObject.getJSONArray("results");
+			cityNameList = new ArrayList<String>(jsonArray.length());
+			
+			//Location object with the current user location.
+			currentLocation = getCurrentLocation();
+			
+			for(int i=0;i<jsonArray.length();i++){
+				if(currentLocation!=null){
+					double latitude = jsonArray.getJSONObject(i).getJSONObject("geo_position").getDouble("latitude");
+					double longitude = jsonArray.getJSONObject(i).getJSONObject("geo_position").getDouble("longitude");
+					
+					//Create location object with lat and long details for each result
+					Location location = new Location(jsonArray.getJSONObject(i).getString("name"));
+					location.setLatitude(latitude);
+					location.setLongitude(longitude);
+					float distance = location.distanceTo(currentLocation);
+					map.put(distance,location.getProvider());
+				}else{
+					cityNameList.add(jsonArray.getJSONObject(i).getString("name"));
+				}
+			}
+		} catch (JSONException e) {
+			System.out.println("JSONException : " + e);
+		}
+		
+		//TreeMap to sort the data in the map according to the closest position
+		// and add city names to the result list.
+		if(currentLocation!=null){
+			Map<Float,String> sortedMap = new TreeMap<Float, String>(map);
+			for (Map.Entry<Float, String> entry : sortedMap.entrySet()){
+				cityNameList.add(entry.getValue());
+			}
+		}
+		
+		return cityNameList;
+	} 
+	
+	private static String getJsonAsString(String urlJson, String input) {
+
+		StringBuilder resultStringBuilder = new StringBuilder();
+		try {
+			URL url = new URL(urlJson + input);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+
+			if (conn.getResponseCode() != 200) {
+				System.out.println("HTTP error code: " + conn.getResponseCode());
+			}
+
+			InputStreamReader in = new InputStreamReader(conn.getInputStream());
+
+			int read;
+			char[] buff = new char[1024];
+			while ((read = in.read(buff)) != -1) {
+				resultStringBuilder.append(buff, 0, read);
+			}
+			conn.disconnect();
+
+		} catch (MalformedURLException e) {
+			System.out.println("MalformedUrlException : " + e);
+		} catch (IOException e) {
+			System.out.println("IOException : " + e);
+		}
+
+		return resultStringBuilder.toString();
+	}
+	
+	private static Location getCurrentLocation(){
+		 LocationManager lm = (LocationManager)mContext.getSystemService(Context.LOCATION_SERVICE); 
+		 Location gpsLocation = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		 Location networkLocation = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);	
+		 Location passiveLocation = lm.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+		 if(gpsLocation != null){
+			 return gpsLocation;
+		 }
+		 if(networkLocation!=null){
+			 return networkLocation;
+		 }		
+		 if(passiveLocation !=null){
+			 return passiveLocation;
+		 }
+		return null;
+	}
+}
---
+++ TravelSearch/src/com/test/travelsearch/MainActivity.java
@@ -0,0 +1,102 @@
+package com.test.travelsearch;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.text.InputType;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+	private Button mSearchButton;
+	private AutoCompleteTextView mAutoStart;
+	private AutoCompleteTextView mAutoDestination;
+	private EditText mDateEditText;
+	private Calendar myCalendar;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_layout);
+		myCalendar = Calendar.getInstance();		
+		
+		// Auto complete edit text's
+		mAutoStart = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView1);
+		mAutoStart.setHint("Enter departure city" );
+		mAutoStart.setAdapter(new AutoCompleteAdapter(this, R.layout.listview_item));
+		mAutoStart.setThreshold(2);
+		
+		mAutoDestination = 	(AutoCompleteTextView) findViewById(R.id.autoCompleteTextView2);
+		mAutoDestination.setHint("Enter destination city" );
+		mAutoDestination.setAdapter(new AutoCompleteAdapter(this, R.layout.listview_item));
+		mAutoDestination.setThreshold(2);		
+		
+		//Search button
+		mSearchButton = (Button) findViewById(R.id.button1);
+		mSearchButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+						v.getContext());
+					alertDialogBuilder.setTitle("Search Results");
+					alertDialogBuilder
+						.setMessage("Search is not yet implemented!!")
+						.setCancelable(false)
+						.setPositiveButton("OK",new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,int id) {
+							}
+						  });
+		 
+						AlertDialog alertDialog = alertDialogBuilder.create();
+						alertDialog.show();
+			}
+		});
+		
+		final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+		    @Override
+		    public void onDateSet(DatePicker view, int year, int monthOfYear,
+		            int dayOfMonth) {
+		        myCalendar.set(Calendar.YEAR, year);
+		        myCalendar.set(Calendar.MONTH, monthOfYear);
+		        myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+		        updateEditText();
+		    }
+
+		};
+		
+		// Edit text for entering date
+			mDateEditText = (EditText) findViewById(R.id.editText1);
+			mDateEditText.setHint("Select date for travel");
+			mDateEditText.setInputType(InputType.TYPE_NULL);
+			updateEditText();
+			mDateEditText.setOnClickListener(new View.OnClickListener() {
+
+		        @Override
+		        public void onClick(View v) {
+		            new DatePickerDialog(v.getContext(), date, myCalendar
+		                    .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+		                    myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+		            
+		        }
+		    });
+	}
+	
+	public void updateEditText() {
+		String myFormat = "EEE, dd MMM yyyy"; // In which you need put here
+		SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+		mDateEditText.setText(sdf.format(myCalendar.getTime()));
+	}
+	
+}
