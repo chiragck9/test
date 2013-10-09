b3c1a402bdc898a96993eac7df0435f103c08cd6
chiragck9 <chirag_ck9@hotmail.com>
2013-10-09 18:48:36 +0530

    layout

--- TravelSearch/gen/com/test/travelsearch/R.java
+++ TravelSearch/gen/com/test/travelsearch/R.java
@@ -42,8 +42,8 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int go_euro=0x7f050003;
         public static final int hello_world=0x7f050002;
+        public static final int travel_search=0x7f050003;
     }
     public static final class style {
         /** 
